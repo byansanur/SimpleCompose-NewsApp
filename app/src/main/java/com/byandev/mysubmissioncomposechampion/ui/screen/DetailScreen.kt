17c1cecbe36914d665f521e034897ed880361f8b
Ratbyansa Nur <byan@sgo.co.id>
2022-11-22 03:33:49 +0700

    ui detail screen

--- app/src/main/java/com/byandev/mysubmissioncomposechampion/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/byandev/mysubmissioncomposechampion/ui/screen/DetailScreen.kt
@@ -1,11 +1,25 @@
 package com.byandev.mysubmissioncomposechampion.ui.screen
 
 import android.util.Log
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.byandev.mysubmissioncomposechampion.model.Articles
-import com.byandev.mysubmissioncomposechampion.model.ArticlesData
 import com.byandev.mysubmissioncomposechampion.ui.theme.MySubmissionComposeChampionTheme
 
 @Composable
@@ -14,7 +28,11 @@ fun DetailScreen(
     modifier: Modifier = Modifier,
     navigateBack: () -> Unit,
 ) {
-    DetailContent(articles = newsData, onBackClick = { navigateBack }, modifier = modifier)
+    DetailContent(
+        articles = newsData,
+        onBackClick = { navigateBack },
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -24,6 +42,80 @@ fun DetailContent(
     modifier: Modifier = Modifier
 ) {
     Log.e("TAG", "DetailContent: ${articles.title}")
+
+    var isExpanded by remember { mutableStateOf(false) }
+
+    Column(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .weight(1f)
+        ) {
+            Box {
+               AsyncImage(
+                   model = articles.urlToImage,
+                   contentDescription = null,
+                   contentScale = ContentScale.Crop,
+                   modifier = Modifier
+                       .size(400.dp)
+                       .clip(RoundedCornerShape(8))
+                       .fillMaxWidth()
+               )
+            }
+            Spacer(modifier = modifier.height(8.dp))
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = articles.title,
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.h5.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ),
+                )
+                Spacer(modifier = modifier.height(4.dp))
+                Row {
+                    Text(
+                        text = articles.publishedAt,
+                        style = MaterialTheme.typography.body2,
+                        textAlign = TextAlign.Start,
+                    )
+                    Text(
+                        text = if (articles.author != "-")" - ${articles.author}" else articles.author,
+                        style = MaterialTheme.typography.body2,
+                        textAlign = TextAlign.Start,
+                    )
+                }
+                Spacer(modifier = modifier.height(4.dp))
+                Text(
+                    text = articles.description,
+                    style = MaterialTheme.typography.subtitle1.copy(
+                        fontWeight = FontWeight.Medium
+                    ),
+                    textAlign = TextAlign.Start,
+                )
+                Text(
+                    text = articles.content,
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.subtitle1.copy(
+                        fontWeight = FontWeight.Medium
+                    ),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    overflow = if (isExpanded) TextOverflow.Visible else TextOverflow.Ellipsis
+                )
+                Text(
+                    text = if (!isExpanded) "Read More" else "",
+                    color = MaterialTheme.colors.secondary,
+                    modifier = modifier.clickable {
+                        isExpanded = true
+                    }
+                )
+
+            }
+            Spacer(modifier = modifier.height(8.dp))
+        }
+    }
 }
 
 @Composable
@@ -31,7 +123,15 @@ fun DetailContent(
 fun PreviewDetailScreen() {
     MySubmissionComposeChampionTheme {
         DetailContent(
-            articles = Articles("aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa"),
+            articles = Articles(
+                "aa",
+                "aaa",
+                "aaaa",
+                "aaaaa",
+                "aaaaaa",
+                "aaaaaaa",
+                "aaaaaaaa"
+            ),
             onBackClick = {}
         )
     }
